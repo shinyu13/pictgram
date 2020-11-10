b0876cdebf9c6f569416c18f7b3d230e97c2a9ce
shinyup213 <shinya@yup.jp>
2020-11-10 16:30:22 +0000

    first commit

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -6,7 +6,6 @@ class Topic < ApplicationRecord
     
     mount_uploader :image, ImageUploader
     
-    belongs_to :user
     has_many :favorites
     has_many :favorite_users, through: :favorites, source: 'user'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,5 @@ class User < ApplicationRecord
     
     has_many :topics
     has_many :favorites
-    has_many :favorites_topics, through: :favorites, source: 'topic'
+    has_many :favorite_topics, through: :favorites, source: 'topic'
 end
